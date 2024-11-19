c93b7fbdfe6313285f8eb78098104aa174975087
Mitanshu Agrawal <mishu10005agrawal@gmail.com>
2024-11-19 22:38:30 +0530

    make a file folder and initilized talwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import { Routes, Route } from 'react-router-dom';
+// import './App.css'
+import './index.css'
 
-import './App.css'
-
+import Chat from './chat/chat.friend.jsx'
 function App() {
   
   return (
-    <></>
+    <>
+      <Routes>
+        <Route path="/chat" element={<Chat/>}/>
+      </Routes>
+    </>
   )
 }
 
--- src/chat/chat.friend.jsx
+++ src/chat/chat.friend.jsx
@@ -3,7 +3,12 @@
 const ChatFriend = () => {
 return( <>
    
-     
+     <div className="bg-slate-600">
+        <div className="flex items-center align-center justify-center p-4 text-red-400">hello</div>
+        <div className="justify-center align-center">Discovered Server</div>
+        <div>Welcome</div>
+        <div>Announcement</div>
+     </div>
 
     </>)
   
